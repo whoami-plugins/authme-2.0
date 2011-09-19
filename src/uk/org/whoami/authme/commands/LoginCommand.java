e43a28f8298d1022a7a349917f2713324d5c2d41
Sebastian Köhler <sebkoehler@whoami.org.uk>
2011-09-19 23:57:02 +0200

    license added

--- src/uk/org/whoami/authme/commands/LoginCommand.java
+++ src/uk/org/whoami/authme/commands/LoginCommand.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Sebastian Köhler <sebkoehler@whoami.org.uk>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.org.whoami.authme.commands;
 
 import java.security.NoSuchAlgorithmException;
@@ -39,7 +55,7 @@ public class LoginCommand implements CommandExecutor {
         Player player = (Player) sender;
         String name = player.getName().toLowerCase();
         String ip = player.getAddress().getAddress().getHostAddress();
-        
+
         if (PlayerCache.getInstance().isAuthenticated(name)) {
             player.sendMessage(m._("logged_in"));
             return true;
